a0af963b83d7683dd49300b96af28701ce8c373f
chalous <chalousdoris@gmail.com>
2021-12-26 02:08:33 +0800

    install the angular and implement a basic function that print the data from the back

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use('/angular', express.static(__dirname + '/node_modules/angular/'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/welcome',function(req,res) {
+  res.send("Hello");
+})
+
 module.exports = router;
